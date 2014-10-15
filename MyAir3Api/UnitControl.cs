c57dd4ca1510471000f17a3dac08d19e63125790
Eric Winkler <eric@winkler.net.au>
2014-10-15 19:29:36 +0800

    Update the unit control (power on\off, temp, fan speed, etc)

--- MyAir3Api/UnitControl.cs
+++ MyAir3Api/UnitControl.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace Winkler.MyAir3Api
@@ -7,11 +8,11 @@ namespace Winkler.MyAir3Api
         private readonly IAirconWebClient _aircon;
 
         public bool Power { get; private set; }
-        public FanSpeed FanSpeed { get; private set; }
-        public InverterMode InverterMode { get; private set; }
+        public FanSpeed FanSpeed { get; set; }
+        public InverterMode InverterMode { get; set; }
         public bool TempsSetting { get; private set; }
         public decimal CentralActualTemp { get; private set; }
-        public decimal CentralDesiredTemp { get; private set; }
+        public decimal CentralDesiredTemp { get; set; }
         public string ErrorCode { get; private set; }
         public string ActivationCodeStatus { get; private set; }
         public int NumberOfZones { get; private set; }
@@ -35,6 +36,15 @@ namespace Winkler.MyAir3Api
             MaxUserTemp = decimal.Parse(data.Element("maxUserTemp").Value);
             MinUserTemp = decimal.Parse(data.Element("minUserTemp").Value);
             NumberOfSchedules = int.Parse(data.Element("availableSchedules").Value);
+        }
+
+        public async Task<AirconWebResponse> UpdateAsync()
+        {
+            return await _aircon.GetAsync("setSystemData?"
+                + "airconOnOff=" + (Power ? "1" : "0")
+                + "&fanSpeed=" + (int) FanSpeed
+                + "&mode=" + (int) InverterMode
+                + "&centralDesiredTemp=" + CentralDesiredTemp);
         }
     }
 }
