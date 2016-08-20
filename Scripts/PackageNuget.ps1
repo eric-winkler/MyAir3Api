e4e066ce1ab596a400228924a5592630c30e0d43
Eric Winkler <eric@winkler.net.au>
2016-08-20 16:18:16 +0800

    Package for netstandard instead of UWP and net45

--- Scripts/PackageNuget.ps1
+++ Scripts/PackageNuget.ps1
@@ -1,9 +1,8 @@
 $parentDirectory = split-path $PSScriptRoot -parent
 $nugetPath = join-path $parentDirectory "tools\nuget.exe"
 $nuspecSource = join-path $PSScriptRoot "MyAir3Api.nuspec"
-$45buildPath = join-path $parentDirectory "MyAir3Api\bin\Release\*"
 $MF43buildPath = join-path $parentDirectory "MFMyAir3Api\bin\Release\*"
-$UAPbuildPath = join-path $parentDirectory "MyAir3Api.UAP\bin\Release\*"
+$netstandardbuildPath = join-path $parentDirectory "MyAir3Api\bin\Release\*"
 
 
 $outputDirectory = join-path $parentDirectory -childpath "nuget output"
@@ -15,19 +14,15 @@ New-Item -ItemType directory -Path $outputDirectory | Out-Null
 $tempName = [System.Guid]::NewGuid().ToString()
 $packagePath = join-path $env:temp $tempName
 $corePackagePath = join-path $packagePath -childpath "core"
-$45libPath = join-path $corePackagePath -childpath "lib\net45"
-$UAPlibPath = join-path $corePackagePath -childpath "lib\netcore45"
 $MF43libPath = join-path $corePackagePath -childpath "lib\netmf43"
+$netstandardlibPath = join-path $corePackagePath -childpath "lib"
 
 New-Item -ItemType directory -Path $packagePath | Out-Null
 New-Item -ItemType directory -Path $corePackagePath | Out-Null
-New-Item -ItemType directory -Path $45libPath | Out-Null
 New-Item -ItemType directory -Path $MF43libPath | Out-Null
-New-Item -ItemType directory -Path $UAPlibPath | Out-Null
 Copy-Item $nuspecSource $corePackagePath
-Copy-Item $45buildPath $45libPath -Recurse
 Copy-Item $MF43buildPath $MF43libPath -Recurse
-Copy-Item $UAPbuildPath $UAPlibPath -Recurse
+Copy-Item $netstandardbuildPath $netstandardlibPath -Recurse
 
 Push-Location -Path $corePackagePath
 
